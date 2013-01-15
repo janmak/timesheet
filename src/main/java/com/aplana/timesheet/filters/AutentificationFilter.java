14fdb066f98bf8e5a148513dcbd467e162548c3c
eshangareev <eshangareev@aplana.com>
2013-01-15 16:43:29 +0600

    Рефакторинг. Фильтры.

--- src/main/java/com/aplana/timesheet/filters/AutentificationFilter.java
+++ src/main/java/com/aplana/timesheet/filters/AutentificationFilter.java
@@ -12,23 +12,24 @@ import java.io.IOException;
  * Time: 14:10
  */
 public class AutentificationFilter implements Filter {
+    @Override
     public void destroy() {
     }
 
+    @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
 
         HttpServletRequest request = (HttpServletRequest) req;
         HttpSession session = request.getSession(false);
         try {
-            String s = request.getParameter("j_username");
-            session.setAttribute("lastLogin", s);
+            session.setAttribute("lastLogin", request.getParameter("j_username") );
         } catch (Exception ex) {
             session.setAttribute("lastLogin", "");
         }
         chain.doFilter(req, resp);
-
     }
 
+    @Override
     public void init(FilterConfig config) throws ServletException {
 
     }
