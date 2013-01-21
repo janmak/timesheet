9787b70038e222794e0462e340b52f0923610aa4
Eugene Kuvshinov <smpl@itmywork.com>
2013-01-21 19:03:33 +0600

    APLANATS-514 APLANATS-510 APLANATS-509 APLANATS-391 [+] Вынесена обработка исключений в отдельное место, осталось подправить внешний вид и сделать чтобы exception'ы уходили на почту

---
+++ src/main/java/com/aplana/timesheet/controller/ExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.aplana.timesheet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class ExceptionHandler implements HandlerExceptionResolver {
+
+    public ModelAndView resolveException(HttpServletRequest request,
+                                         HttpServletResponse response, Object handler, Exception exception)
+    {
+        Map<String, Object> model = new HashMap<String, Object>();
+        if (exception instanceof MaxUploadSizeExceededException)
+        {
+            model.put("exceptionText", "error.feedback.maxsize");
+            return new ModelAndView("redirect:feedback", model);
+        } else
+        {
+            model.put("errors", "Unexpected error: " + exception.getMessage());
+        }
+        return new ModelAndView("exception", model);
+    }
+}
