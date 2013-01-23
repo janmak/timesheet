ce321907075198390e7980f8b100099276e379b0
vsergeev <vsergeev@aplana.com>
2013-01-23 10:32:06 +0600

    исправил опечатку

--- src/main/java/com/aplana/timesheet/dao/CalendarDAO.java
+++ src/main/java/com/aplana/timesheet/dao/CalendarDAO.java
@@ -144,7 +144,7 @@ public class CalendarDAO {
     /**
      * Возвращает количество выходных дней за выбранный период для конкретного региона
      */
-    public Integer getHolydaysCountForRegion(Date beginDate, Date endDate, Region region){
+    public Integer getHolidaysCountForRegion(Date beginDate, Date endDate, Region region){
         Query query = entityManager.createQuery("select count (*) from Holiday as h where ((h.calDate.calDate between :beginDate and :endDate) and (h.region is null or h.region = :region))");
         query.setParameter("beginDate", beginDate);
         query.setParameter("endDate", endDate);
