ac228975bb7c7d46a43c2d5254658c3ede212390
akruzhkov <akruzhkov@aplana.com>
2013-05-29 17:56:27 +0600

    APLANATS-800

--- src/main/java/com/aplana/timesheet/controller/report/Report01ModelAndViewGenerator.java
+++ src/main/java/com/aplana/timesheet/controller/report/Report01ModelAndViewGenerator.java
@@ -1,13 +1,9 @@
 package com.aplana.timesheet.controller.report;
 
-import com.aplana.timesheet.enums.OvertimeCategory;
 import com.aplana.timesheet.reports.Report01;
 import com.aplana.timesheet.reports.TSJasperReport;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author eshangareev
@@ -19,12 +15,6 @@ public class Report01ModelAndViewGenerator extends AbstractJasperReportModelAndV
     @Override
     protected void fillSpecificProperties( ModelAndView mav ) {
         fillDivisionList( mav );
-
-        List<OvertimeCategory> ls = new ArrayList<OvertimeCategory>();
-        ls.add(OvertimeCategory.ALL);
-        ls.add(OvertimeCategory.HOLIDAY);
-        ls.add(OvertimeCategory.SIMPLE);
-        mav.addObject("categoryList", ls);
     }
 
     @Override
