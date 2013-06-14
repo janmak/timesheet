3209562edd9b5e0f50852473b93371faae6742a1
Sergey Kotov <kssgel@yandex.ru>
2013-06-14 14:03:54 +0600

    Создание сессии, если еще нет

--- src/main/java/com/aplana/timesheet/filters/AutentificationFilter.java
+++ src/main/java/com/aplana/timesheet/filters/AutentificationFilter.java
@@ -2,7 +2,6 @@ package com.aplana.timesheet.filters;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
@@ -20,7 +19,7 @@ public class AutentificationFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
 
         HttpServletRequest request = (HttpServletRequest) req;
-        HttpSession session = request.getSession(false);
+        HttpSession session = request.getSession(true);
         try {
             session.setAttribute("lastLogin", request.getParameter("j_username") );
         } catch (Exception ex) {
