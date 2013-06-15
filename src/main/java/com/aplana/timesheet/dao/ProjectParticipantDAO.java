732f61d5c243ee788c98a20ae9c409d98b8ad580
gbax <archy1188@yandex.ru>
2013-06-15 17:14:09 +0600

    APLANATS-839 При синхронизации с ldap обновляется таблица project_participant (http://jira.aplana.com/browse/APLANATS-839)

--- src/main/java/com/aplana/timesheet/dao/ProjectParticipantDAO.java
+++ src/main/java/com/aplana/timesheet/dao/ProjectParticipantDAO.java
@@ -3,15 +3,21 @@ package com.aplana.timesheet.dao;
 import com.aplana.timesheet.dao.entity.Employee;
 import com.aplana.timesheet.dao.entity.Project;
 import com.aplana.timesheet.dao.entity.ProjectParticipant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.util.List;
 
 
 @Repository
 public class ProjectParticipantDAO {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProjectParticipantDAO.class);
+
 	@PersistenceContext
 	private EntityManager entityManager;
 	
@@ -24,6 +30,19 @@ public class ProjectParticipantDAO {
                 "where p.project = :project and p.employee = :employee")
                 .setParameter("project", project).setParameter("employee", employee);
         return query.getResultList().size() != 0;
+    }
+
+    public void save(ProjectParticipant participant)  {
+        entityManager.merge(participant);
+        entityManager.flush();
+        logger.info("Persistence context synchronized to the underlying database.");
+        logger.debug("Flushed participant object id = {}", participant.getId());
+    }
+
+    public List<ProjectParticipant> findByEmployee(Employee employee) {
+        final Query query = entityManager.createQuery("from ProjectParticipant p " +
+                "where p.employee = :employee").setParameter("employee", employee);
+        return query.getResultList();
     }
 
 }
