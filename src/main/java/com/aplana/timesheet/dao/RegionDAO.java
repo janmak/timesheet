4dadd787d902dfcfb7c0c677f38033b8a4b84a54
gbax <archy1188@yandex.ru>
2013-06-21 16:15:01 +0600

    APLANATS-674 Сменить сортировку регионов на форме планирования занятости (http://jira.aplana.com/browse/APLANATS-674)

--- src/main/java/com/aplana/timesheet/dao/RegionDAO.java
+++ src/main/java/com/aplana/timesheet/dao/RegionDAO.java
@@ -1,6 +1,8 @@
 package com.aplana.timesheet.dao;
 
+import com.aplana.timesheet.dao.entity.Project;
 import com.aplana.timesheet.dao.entity.Region;
+import com.aplana.timesheet.enums.RegionsEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
@@ -8,7 +10,12 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @Repository
@@ -45,7 +52,17 @@ public class RegionDAO {
 	}
 
     @SuppressWarnings("unchecked")
-	public List<Region> getRegions() {
-        return ( List<Region> ) entityManager.createQuery("from Region as r").getResultList();
-	}
+    public List<Region> getRegions() {
+        List<Region> regions = (List<Region>) entityManager.createQuery("from Region as r order by r.name").getResultList();
+        Region region = null;
+        for (int i = 0; i <= regions.size(); i++) {
+            if (regions.get(i).getId().equals(RegionsEnum.OTHERS.getId())) {
+                region = regions.get(i);
+                regions.remove(i);
+                break;
+            }
+        }
+        regions.add(regions.size(),region);
+        return regions;
+    }
 }
