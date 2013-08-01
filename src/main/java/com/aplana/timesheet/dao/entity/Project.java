d33c3a0644860d60a72f44354441caf35636ef43
niklebedev <Nikolay.Lebedev@aplana.com>
2013-08-01 10:38:08 +0600

    В рамках задачи http://jira.aplana.com/browse/APLANATS-822 добавление поля jira project key + скрипт на БД

--- src/main/java/com/aplana/timesheet/dao/entity/Project.java
+++ src/main/java/com/aplana/timesheet/dao/entity/Project.java
@@ -60,6 +60,9 @@ public class Project {
     @Column(name = "end_date", columnDefinition = "date")
     private Date endDate;
 
+    @Column(name = "jira_project_key")
+    private String jiraProjectKey;
+
 	public Set<Division> getDivisions() {
 		return divisions;
 	}
@@ -148,6 +151,14 @@ public class Project {
         this.endDate = endDate;
     }
 
+    public String getJiraProjectKey() {
+        return jiraProjectKey;
+    }
+
+    public void setJiraProjectKey(String jiraProjectKey) {
+        this.jiraProjectKey = jiraProjectKey;
+    }
+
     @Override
 	public String toString() {
 		return new StringBuilder()
@@ -173,4 +184,5 @@ public class Project {
     public void setEmployeeProjectBillables(Set<EmployeeProjectBillable> employeeProjectBillables) {
         this.employeeProjectBillables = employeeProjectBillables;
     }
+
 }
